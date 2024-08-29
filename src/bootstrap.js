423bd74de3df8789b0cd9a2dd7c5546b6bdfc6e9
AinhoaAlonso <ainhoaalonso88@gmail.com>
2024-08-29 12:51:52 +0200

    Creamos las rutas dentro de boostrap

--- src/bootstrap.js
+++ src/bootstrap.js
@@ -2,19 +2,26 @@ import React from "react";
 import ReactDOM from "react-dom";
 import { Provider } from "react-redux";
 import { createStore, applyMiddleware } from "redux";
-import { BrowserRouter } from "react-router-dom";
-import App from "./components/app";
+import { BrowserRouter, Switch, Route} from "react-router-dom";
+
 import reducers from "./reducers";
 
 const createStoreWithMiddleware = applyMiddleware()(createStore);
 
+import Home from "./components/home";
+
 import "./style/main.scss";
+import RecentPosts from "./components/recentPosts";
 
 function main() {
   ReactDOM.render(
     <Provider store={createStoreWithMiddleware(reducers)}>
       <BrowserRouter>
-        <App />
+        {/*Vamos a definir aqui las rutas*/}
+        <Switch>
+          <Route path = "/" component={Home} />
+          <Route path = "/recentPosts" component={RecentPosts} />
+        </Switch>
       </BrowserRouter>
     </Provider>,
     document.querySelector(".app-wrapper")
