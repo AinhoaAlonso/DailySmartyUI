c9c01fffc36993645dabda8e84758f967b98a182
AinhoaAlonso <ainhoaalonso88@gmail.com>
2024-09-05 17:57:25 +0200

    Insalacion react-animate-height

--- src/components/post.js
+++ src/components/post.js
@@ -1,7 +1,16 @@
 //Con este componente voy a recuperar los datos que quiero coger de la API para mostrarlos en la pantalla de inicio, los 3 posts
 import React, {Component} from "react";
+import AnimateHeight from "react-animate-height";
 
 export default class Post extends Component{
+    constructor(props){
+        super(props);
+
+        this.state = {
+            //Por defecto la altura a cero que va a cambiar a medida que vengan los links de los planets
+            height:0
+        }
+    }
     //Esto solo se puede hacer si tuviesemos un array, lo dejo en el código por si necesito mirarlo en algun momento, pruebo con uno de nuestra API, esto seria en la barra de la home
     /*renderPlanets(){
         let planets = this.props.planets.map((planet, index) => {
@@ -46,12 +55,24 @@ export default class Post extends Component{
                     <div className="result-post-director">
                         {this.props.director}
                     </div>
-                    <div className="results-post-title">
-                        {this.props.title}
+                    {/*Aquí lo que vamos a hacer es que cuando pongas el raton encima del titulo abre una animacion de desplazamiento hacia abajo con los links de los planets y luego quitas el raton la quita */}
+                    {/*Le amplia la altura a 100 y luego la baja a cero */}
+                    <div className="results-post-title"
+                    onMouseEnter={() => this.setState({height: 100})}
+                    onMouseLeave={() => this.setState({height: 0})}
+                    >
+                        {/*Hacemos que el titulo sea un enlace de la url*/}
+                        <a href={this.props.url}>{this.props.title}</a>
                     </div>
-                    <div className="results-post-url">
-                        {this.renderPlanets()}
-                    </div>
+                    {/*La animación la ponemos para los links de los planets */}
+                    <AnimateHeight
+                        duration={500}
+                        height={this.state.height}
+                    >
+                        <div className="results-post-url">
+                            {this.renderPlanets()}
+                        </div>
+                    </AnimateHeight>
                 </li>
             )
         }
